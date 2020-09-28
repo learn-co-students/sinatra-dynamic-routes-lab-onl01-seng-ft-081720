e0a72e46b7f4c5c24975622c78d679176ce2c448
atBeatty <adam.t.beatt@gmail.com>
2020-09-28 11:55:19 -0700

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,49 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/' do 
+    "Hello"
+  end
+
+  get '/reversename/:name' do
+    @name = params[:name].to_s.reverse 
+    "#{@name}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    # response = @number.to_i*@number.to_i
+    "#{@number*@number}"
+
+  end
+  
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase].to_s
+    response = ''
+    @number.times do
+      response += @phrase
+    end
+    "#{response}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params.values.join(" ")}."
+  end
+
+  get '/:operation/:number1/:number2' do
+  @number1 = params[:number1].to_i
+  @number2 = params[:number2].to_i
+
+    if params[:operation] == "add"
+      "#{@number1 + @number2}"
+    elsif params[:operation] == "subtract"
+      "#{@number1 - @number2}"
+    elsif params[:operation] == "multiply"
+      "#{@number1 * @number2}"
+    elsif params[:operation] == "divide"
+      "#{@number1 / @number2}"
+    end
+  end
 
 end
